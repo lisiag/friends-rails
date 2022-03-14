ce01bd54cd40f8a46b4ac87e80e23d446191a24a
Lisia Grocott <lisiagrocott@gmail.com>
2022-03-14 20:48:03 +1300

    switch db from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_28_073751) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
