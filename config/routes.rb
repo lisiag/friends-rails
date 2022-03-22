b4cdcc9b9c820d23b893eda587972a611dbc9dce
Lisia Grocott <lisiagrocott@gmail.com>
2022-03-23 11:04:03 +1300

    generate devise user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users, :path_prefix => 'my'
+  devise_for :users, :path_prefix => 'my', :controllers => { registrations: 'users/registrations' }
 
   # resources :users do
   #   member do
